877f9a298aa8f5e19aee7bef24b5b58fd62702bd
mragimov29 <mragimov29@gmail.com>
2022-12-24 01:43:12 +0400

    fix functional to converter

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -7,48 +7,55 @@ export default function Converter({ symbol, id }) {
   const [currency, setCurrency] = useState(null);
   const [first, setFirst] = useState(null);
   const [second, setSecond] = useState(null);
-  const [selectValue, setSelectValue] = useState('usd');
+  const [selectValue, setSelectValue] = useState("usd");
 
-  const getData = async (vs_currency) => {
-    let response = await fetch(
+  const getData = (vs_currency) => {
+    let response = fetch(
       `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${vs_currency}&days=0`
     );
     let data = response.json();
     return data;
   };
 
-  const getCurrency = (value) => {
-    getData(value)
-    .then((res) => {
-      setCurrency(res.prices[0][1]);
-    })
-    .catch((err) => alert(err));
-    setOneTime(false);
-  }
+  const getCurrency = (vs_currency) => {
+    fetch(
+        `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${vs_currency}&days=0`
+      ).then(res=>res.json())
+      .then(data => {
+        setCurrency(data.prices[0][1]);
+      })
+    // getData(value)
+    //   .then((res) => {
+    //     setCurrency(res.prices[0][1]);
+    //     console.log('res', res.prices[0][1]);
+    //   })
+    //   .catch((err) => alert(err));
+    // setOneTime(false);
+  };
 
   useEffect(() => {
     if (oneTime) {
-        getCurrency(selectValue);
-        console.log(currency)
+      getCurrency(selectValue);
+      console.log(currency);
     }
   }, []);
 
   const firstChange = (e) => {
     setFirst(e.target.value);
-    setSecond((e.target.value*currency).toFixed(2))
-  }
+    setSecond((e.target.value * currency).toFixed(2));
+  };
 
   const secondChange = (e) => {
     setSecond(e.target.value);
-    setFirst((e.target.value/currency).toFixed(2))
-  }
+    setFirst((e.target.value / currency).toFixed(2));
+  };
 
   const selectChange = (e) => {
-    console.log(e.target.value)
     getCurrency(e.target.value);
     setSelectValue(e.target.value);
-    setSecond((first*currency).toFixed(2));
-  }
+    // setSecond((first * currency).toFixed(2));
+    secondChange();
+  };
 
   return (
     <div>
@@ -60,17 +67,24 @@ export default function Converter({ symbol, id }) {
         </div>
 
         <div className="second">
-          <select value={selectValue} onChange={selectChange} className="custom-dropdown">
-            <option value="usd">
-              USD
-            </option>
+          <select
+            value={selectValue}
+            onChange={selectChange}
+            className="custom-dropdown"
+          >
+            <option value="usd">USD</option>
             <option value="eur">EUR</option>
             <option value="btc">BTC</option>
             <option value="eth">ETH</option>
             <option value="bnb">BNB</option>
             <option value="bch">BCH</option>
           </select>
-          <input value={second} onChange={secondChange} type="number"></input>
+          <input
+            value={second}
+            onChange={secondChange}
+            className="second-input"
+            type="number"
+          ></input>
         </div>
       </div>
     </div>
