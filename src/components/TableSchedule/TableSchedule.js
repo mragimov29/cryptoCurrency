579211dfdcc7dc7eeb1157b344c7ec10b696f52f
qquliyevvv <quluzade228@gmail.com>
2022-12-23 13:36:32 +0400

    add filling the dates

--- src/components/TableSchedule/TableSchedule.js
+++ src/components/TableSchedule/TableSchedule.js
@@ -3,20 +3,36 @@ import { connect } from "react-redux";
 import { Line } from "react-chartjs-2";
 import { createDates } from "../../redux/actions/actions";
 
-const mapDispatchToProps = (dispatch) => ({
-  createDates: (date, price) => dispatch(createDates(date, price)),
-});
+// const mapDispatchToProps = (dispatch) => ({
+//   createDates: (date, price) => dispatch(createDates(date, price)),
+// });
 
-const mapStateToProps = (state) => {
-  return {
-    dates: state.dates,
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     dates: state.dates,
+//   };
+// };
 
-function TableSchedule({ price, id, date, dates, createDates }) {
+export default function TableSchedule({ price, id, date }) {
+  const [dates, setDates] = useState([]);
+  const [flag, setFlag] = useState(true);
+
   useEffect(() => {
-      createDates(date, price);
-  }, [dates]);
+    if(flag) {
+      const date1 = new Date(date);
+
+      for (let i = 0; i < price.price.length; i++) {
+        date1.setHours(date1.getHours() - 1);
+        // let arrDate = new Date(
+        //   `${date.getDay()}.${date.getMonth()} ${date.getHours()}:${date.getMinutes()}`
+        // );
+        dates.unshift(date1);
+      }
+  
+      console.log(id, dates);
+      setFlag(false);
+    }
+  });
 
   // const data = {
   //   label: dates,
@@ -25,4 +41,4 @@ function TableSchedule({ price, id, date, dates, createDates }) {
   // return <Line data={data} options={options} />;
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(TableSchedule);
+// export default connect(mapStateToProps, mapDispatchToProps)(TableSchedule);
