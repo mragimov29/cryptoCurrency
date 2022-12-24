dab14d5b85f5fea076723093d7d71be364d75ae9
mragimov29 <mragimov29@gmail.com>
2022-12-24 20:48:21 +0400

    add search answer request

--- src/components/SearchAnswer/SearchAnswer.js
+++ src/components/SearchAnswer/SearchAnswer.js
@@ -1,16 +1,20 @@
 import "./SearchAnswer.css";
 
-export default function SearchAnswer() {
+export default function SearchAnswer({ search, data }) {
+  console.log(data);
+  if (data === null) return true;
+
   return (
-    <div className="search-answer">
-      <ul>
-        <li>ASDFGHj</li>
-        <li>ASDFGHj</li>
-        <li>ASDFGHj</li>
-        <li>ASDFGHj</li>
-        <li>ASDFGHj</li>
-        <li>ASDFGHj</li>
-      </ul>
+    <div className={search ? "search-answer" : "search-none"}>
+      {data.coins.map((item) => {
+        return (
+          <div className="result">
+            <img className="result-img" src={item.thumb}></img>
+            <div>{item.name}</div>
+            <div>{item.symbol}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/components/SearchLine/SearchLine.js
+++ src/components/SearchLine/SearchLine.js
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import SearchAnswer from "../SearchAnswer/SearchAnswer";
 import "./SearchLine.css"
 
 export default function SearchLine() {
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState(null)
+
+  const getData = async (q) => {
+    let response = await fetch(
+      `https://api.coingecko.com/api/v3/search?query=${q}`
+    );
+    
+    let data = response.json();
+    return data;
+  };
+
+  const onChange = (event) => {
+    setSearch(event.target.value);
+    getData(event.target.value)
+        .then((res) => {
+          setData(null);
+          setData(res);
+        })
+        .catch((err) => alert(err));
+  }
+
   return (
     <div className="search-line">
-        <input type="search" placeholder="Seach" className="search-line-input" />
-        <SearchAnswer />
+        <input value={search} onChange={onChange} type="search" placeholder="Seach" className="search-line-input" />
+        <SearchAnswer search={search} data={data}/>
     </div>
   );
 }
