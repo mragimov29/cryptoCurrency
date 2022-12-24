c3c2557246805b2868a09147847762c9f67dbfec
mragimov29 <mragimov29@gmail.com>
2022-12-24 23:28:50 +0400

    add search answer roating

--- src/components/SearchAnswer/SearchAnswer.js
+++ src/components/SearchAnswer/SearchAnswer.js
@@ -1,15 +1,21 @@
+import { Link } from "react-router-dom";
 import "./SearchAnswer.css";
 
 export default function SearchAnswer({ search, data }) {
-  console.log(data);
-  if (data === null) return true;
+  const reloadPage = () => {
+   
+    setTimeout(() => { window.location.reload(); }, 200);
+  } 
 
+  if (data === null) return true;
   return (
     <div className={search ? "search-answer" : "search-none"}>
       {data.coins.map((item) => {
         return (
           <div className="result">
-            <img className="result-img" src={item.thumb}></img>
+            <Link onClick={reloadPage} to={`/${item.id}`}>
+                <img className="result-img" src={item.thumb}></img>
+            </Link>
             <div>{item.name}</div>
             <div>{item.symbol}</div>
           </div>
