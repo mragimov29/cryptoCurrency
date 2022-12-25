20c61466d0e2908c12b9345034a2144b7828d92d
mragimov29 <mragimov29@gmail.com>
2022-12-26 02:01:40 +0400

    add coin change in buy sell

--- src/components/BuySell/BuySell.js
+++ src/components/BuySell/BuySell.js
@@ -11,15 +11,20 @@ const mapStateToProps = (state) => {
 function BuySell({ favorites }) {
   const [count, setCount] = useState(0);
   const [id, setId]= useState(favorites[0].data.id);
+//   const [selectValue, setSelectValue]= useState(favorites[0].data.id);
 
   const inputCount = (e) => {
     setCount(e.target.value.replace(/[^0-9]/g, ''));
   }
 
+  const selectCoin = (e) => {
+    setId(e.target.value);
+  }
+
   return (
     <div className="buy-sell">
-      <select className="buy-sell-select">{favorites.map(data => {
-        return <option>
+      <select value={id} onChange={selectCoin} className="buy-sell-select">{favorites.map(data => {
+        return <option value={data.data.id}>
             <p>{data.data.name}</p>
             <p>({data.data.symbol.toUpperCase()})</p>
         </option>
