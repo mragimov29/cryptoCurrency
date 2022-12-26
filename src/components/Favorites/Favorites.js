a67175eecacb6e7c86d78e3f4018bbbba9216b9f
mragimov29 <mragimov29@gmail.com>
2022-12-26 19:31:31 +0400

    add profile page mobile styles

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -12,6 +12,8 @@ function Favorites({ favorites }) {
   useEffect(() => {
     if (favorites.length > 8)
       document.querySelector(".favorites").style.overflowY = "scroll";
+    if (favorites.length > 2  && window.innerWidth <= 550)
+      document.querySelector(".favorites").style.overflowY = "scroll";
   });
 
   if (!favorites) return <>NO!</>;
@@ -25,12 +27,16 @@ function Favorites({ favorites }) {
               className="favorites-star"
               src={require("../../stars/golden_star.svg").default}
             ></img>
-            <div className="favorites-info-li"> 
+            <div className="favorites-info-li">
               <img className="favorites-img" src={data.data.image.small}></img>
-              <p>{data.data.name}</p>
-              <p>({data.data.symbol.toUpperCase()})</p>
-              <p>{data.count}</p>
-              <p>${data.price.toFixed(1)}</p>
+              <div className="favorites-name-symbol">
+                <p>{data.data.name}</p>
+                <p>({data.data.symbol.toUpperCase()})</p>
+              </div>
+              <div className="favorites-count-price">
+                <p>{data.count}</p>
+                <p>${data.price.toFixed(1)}</p>
+              </div>
             </div>
           </div>
         );
